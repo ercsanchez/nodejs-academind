86deae36ef49d0a5482a919c3b8638fd1eae756b
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2022-06-27 08:34:45 +0800

    middleware and basic routing

--- app.js
+++ app.js
@@ -1,9 +1,42 @@
-const http = require('http');
+// const http = require('http');
 
 const express = require('express');
+const bodyParser = require('body-parser'); // already added with expresse but removed before so safer to just install 3rd-party package
 
 const app = express();
 
-const server = http.createServer(app);
+app.use(bodyParser.urlencoded({ extended: false })); // parses body of all requests
 
-server.listen(3000);
+// app.use('/', (req, res, next) => {
+//   console.log('This middleware always runs');
+//   next(); // pass the same req on to next middleware to be able to use it for res
+// });
+
+app.get('/add-product', (req, res) => {
+  console.log('In /add-product middleware');
+  res.send(`
+    <form action="/product" method="POST">
+      <input type="text" name="title" />
+      <button type="submit">Add Product</button>
+    </form>
+  `);
+});
+
+app.post('/product', (req, res) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
+app.use('/', (req, res) => {
+  console.log('In / middleware'); // also executes for '/add-product' since also a match
+  res.send('<h1>Hello from Express!</h1>'); // won't execute for '/add-product'
+});
+
+// const server = http.createServer(app);
+// server.listen(3500, () => {
+//   console.log('Server listening on port 3500!');
+// });
+
+app.listen(3500, () => {
+  console.log('Server listening on port 3500!');
+});
