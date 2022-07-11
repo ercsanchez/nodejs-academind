cdd759df3793e7203f0adb7c97269b6a46e7f9e3
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2022-07-11 11:37:38 +0800

    create and use a dummy user (User model instance)

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
 const { userInfo } = require('os');
+const { nextTick } = require('process');
 
 // const pathTo404Html = path.join(__dirname, 'views', '404.html');
 
@@ -33,6 +34,22 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: false })); // parses body of all requests
 app.use(express.static(path.join(__dirname, 'public')));
 
+// app.use only registers the middleware and doesn't execute the callback func passed to it
+// npm start executes the app.use and sequelize.sync
+// callback func is only executed for incoming requests or when you finally go through a
+// route (after app is running), so there will be no error even when a User is nonexistent
+// before running app since it will be created before we get to a route
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      console.log('currentUser', user.dataValues);
+      // user obj here includes all methods and props of sequelize User model instance
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.error(err));
+});
+
 // test postgresql connection | query all rows of test_table
 app.use('/pgtest', (req, res) => {
   db.query('SELECT * FROM products ORDER BY id ASC')
@@ -54,7 +71,20 @@ User.hasMany(Product);
 sequelize
   .sync({ alter: true })
   .then((result) => {
-    console.log(result);
+    return User.findByPk(1);
+  })
+  .then((user) => {
+    if (!user) {
+      console.log('no user found', user);
+      return User.create({ name: 'John Doe', email: 'johndoe@mail.com' });
+    }
+    // in order to make sure that user is still a promise to be consistent
+    // if a user is created in the if block above, however this is optional, since
+    // anything returned inside of a then block is automatically wrapped in a promise
+    return Promise.resolve(user);
+  })
+  .then((user) => {
+    // console.log(user.dataValues);
     app.listen(3500, () => {
       console.log('Server listening on port 3500!');
     });
